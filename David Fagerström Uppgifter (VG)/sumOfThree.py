0734cfa5c2b580ddb7643231a6cbeaba39a0d1a6
Nojdaz <Nojdaz@gmail.com>
2021-09-07 23:17:49 +0200

    Update to folder structure
    This update was made to categorize the scripts after the supposed difficulty level.

--- David Fagerström Uppgifter (VG)/sumOfThree.py
+++
@@ -1,6 +0,0 @@
-threeDigitNumber = input("Enter a three digit number: ")
-sum = 0
-for i in range(0, 3):
-    sum = sum + int(threeDigitNumber[i])
-print(sum)
-    
--- VG/squareColor.py
+++ VG/squareColor.py
@@ -1,4 +1,3 @@
-
 coordinate = input("Enter a chess square identifier (e.g. e5):")
 
 if (ord(coordinate[0]) + int(coordinate[1])) % 2:
